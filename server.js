b3373a294f87c8af13f8d03eb8d11bdf6c4fcc22
niccoryan0 <niccoryan0@gmail.com>
2020-05-26 11:52:38 -0700

    updated readme and reuqirement

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+  // Remember me info, check if info in local storage on initial page load, SQL query to get their info if in local storage
   // if (req.body.newUser) {
-
   //   const sqlQuery = `INSERT INTO table (username, password, location) VALUES ($1, $2, $3)`;
   //   const valArr = [req.body.newName, req.body.newPass, `${req.body.locationCity}, ${req.body.locationState}`];
   //   client.query(sqlQuery, valArr)
@@ -48,32 +48,32 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-function Weather(obj){
-  this.forecast = obj.weather.description;
-  this.time = new Date(obj.ts * 1000).toDateString();
-}
-Weather.Query = function(req) {
-  this.key = process.env.WEATHER_API_KEY;
-  this.lat = req.query.latitude;
-  this.lon = req.query.longitude;
-};
+// function Weather(obj){
+//   this.forecast = obj.weather.description;
+//   this.time = new Date(obj.ts * 1000).toDateString();
+// }
+// Weather.Query = function(req) {
+//   this.key = process.env.WEATHER_API_KEY;
+//   this.lat = req.query.latitude;
+//   this.lon = req.query.longitude;
+// };
 
-function superAgentWeather(req, res){
-  const apiUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
-  const queryParams = {
-    key : process.env.WEATHER_API_KEY,
-    lat : req.query.latitude,
-    lon : req.query.longitude,
-    days : 7,
-  };
-  superagent.get(apiUrl)
-    .query(queryParams)
-    .then(result => {
-      const newWeather = result.body.data.map(obj => new Weather(obj));
-      return newWeather;
-    });
+// function superAgentWeather(req, res){
+//   const apiUrl = 'https://api.weatherbit.io/v2.0/forecast/daily';
+//   const queryParams = {
+//     key : process.env.WEATHER_API_KEY,
+//     lat : req.query.latitude,
+//     lon : req.query.longitude,
+//     days : 7,
+//   };
+//   superagent.get(apiUrl)
+//     .query(queryParams)
+//     .then(result => {
+//       const newWeather = result.body.data.map(obj => new Weather(obj));
+//       return newWeather;
+//     });
 
-}
+// }
 
 
 
